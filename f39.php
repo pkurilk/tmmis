79e98bef0c360d9a7b2e44d968091c1f0b273305
pkurilk <53551185+pkurilk@users.noreply.github.com>
2019-10-14 16:05:58 +0600

    Create f39.php

---
+++ f39.php
@@ -0,0 +1,99 @@
+<style type="text/css">
+
+
+
+    .table>tbody>tr>td, .table>tbody>tr>th {
+        padding: 4px;
+        line-height: 1.22857;
+        vertical-align: middle;
+        border: 1px solid #777777;
+        min-width: 50px;
+        max-width: 200px;
+        text-align: right;
+        font-size: 12px;
+    }
+
+    .table>tbody>tr>td:first-child{
+        min-width: 180px;
+        text-align: left;
+    }
+
+    .t-h {
+        font-size:12px;
+        border: 1px solid #777777; /* Параметры рамки */
+        text-align:center; /* Выравнивание по левому краю */
+        border-collapse: collapse; /* Убираем двойные линии между ячейками */
+    }
+    
+</style>
+
+<tr class="t-h">
+    <td rowspan="4" class="t-h">ФИО врача</td>
+    <td colspan="4" class="t-h">Число посещений в поликлинике</td>
+    <td colspan="6" class="t-h">Из общего числа посещений в поликлинике</td>
+    <td rowspan="4" class="t-h">Число посещений на дому (всего)</td>
+    <td colspan="9" class="t-h">Из общего числа посещений на дому</td>
+    <td colspan="4" class="t-h">Число посещений по видам оплаты</td>
+</tr>
+<tr class="t-h">
+    <td rowspan="3" class="t-h">всего</td>
+    <td rowspan="3" class="t-h">из них сельских жителей</td>
+    <td colspan="2" class="t-h">В том числе в возрасте  (из графы 2)</td>
+    <td colspan="3" class="t-h"> по поводу заболеваний</td>
+    <td rowspan="3" class="t-h">профилактических</td>
+    <td rowspan="3" class="t-h">число посещений по неотложным состояниям (из гр. 6)</td>
+    <td rowspan="3" class="t-h">паллиативная помощь (из гр. 6)</td>
+    <td colspan="4" class="t-h">По поводу заболеваний</td>
+    <td colspan="3" rowspan="2" class="t-h">из числа профилак-тических</td>
+    <td rowspan="3" class="t-h">число посещений по неотложным состояниям (из гр. 13)</td>
+    <td rowspan="3" class="t-h">паллиативная помощь (из гр. 13)</td>
+    <td rowspan="3" class="t-h">ОМС</td>
+    <td rowspan="3" class="t-h">бюджет</td>
+    <td rowspan="3" class="t-h">платные</td>
+    <td rowspan="3" class="t-h">ДМС</td>
+</tr>
+<tr class="t-h">
+    <td rowspan="2" class="t-h">0 - 17 лет</td>
+    <td rowspan="2" class="t-h">60 лет и старше</td>
+    <td rowspan="2" class="t-h">всего</td>
+    <td colspan="2" class="t-h">в том числе в возрасте</td>
+    <td rowspan="2" class="t-h">всего</td>
+    <td colspan="3" class="t-h">в том числе в возрасте</td>
+</tr>
+<tr class="t-h">
+    <td class="t-h">0 - 17 лет</td>
+    <td class="t-h">60 лет и страше</td>
+    <td class="t-h" >0 - 17 лет</td>
+    <td class="t-h">из них   0 - 1 год (вкл.)</td>
+    <td class="t-h">60 лет и старше</td>
+    <td class="t-h">всего</td>
+    <td class="t-h">0 - 17 лет</td>
+    <td class="t-h">из них 0 - 1 год (вкл.)</td>
+</tr>
+<tr class="t-h">
+    <td class="t-h">1</td>
+    <td class="t-h">2</td>
+    <td class="t-h">3</td>
+    <td class="t-h">4</td>
+    <td class="t-h">5</td>
+    <td class="t-h">6</td>
+    <td class="t-h">7</td>
+    <td class="t-h">8</td>
+    <td class="t-h">9</td>
+    <td class="t-h">10</td>
+    <td class="t-h">11</td>
+    <td class="t-h">12</td>
+    <td class="t-h">13</td>
+    <td class="t-h">14</td>
+    <td class="t-h">15</td>
+    <td class="t-h">16</td>
+    <td class="t-h">17</td>
+    <td class="t-h">18</td>
+    <td class="t-h">19</td>
+    <td class="t-h">20</td>
+    <td class="t-h">21</td>
+    <td class="t-h">22</td>
+    <td class="t-h">23</td>
+    <td class="t-h">24</td>
+    <td class="t-h">25</td>
+</tr>
